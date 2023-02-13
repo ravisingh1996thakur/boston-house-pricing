fd521998baff0cee4f171dc74556632148bd57e8
RAVI SINGH <ravisingh@RAVIs-MacBook-Air.local>
2023-02-13 20:23:21 +0530

    files updated

--- app.py
+++ app.py
@@ -26,6 +26,20 @@ def predict_api():
 
 
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.from_values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input())[0]
+    return render_template('home.html',prediction_text="The house price prediction  is {}".format(output))
+    
+
+
+
+
+
+
 if __name__=="__main__":
     app.run(debug=True)
 
